5317b791906346cdf2a6afd6986e4ef2639fedbf
Shresth <anandshrestha01@gmail.com>
2018-08-29 12:42:09 +0530

    Saving Data while orientation for old version and new version

--- app/src/main/java/com/example/pc/loginapp/view/ui/From.java
+++ app/src/main/java/com/example/pc/loginapp/view/ui/From.java
@@ -11,12 +11,22 @@ public class From extends AppCompatActivity  {
     private EditText name;
     private EditText age;
 
+    public static final String SAVED_TEXT_KEY="SavedText";
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form);
 
         name=(EditText)findViewById(R.id.name);
         age=(EditText) findViewById(R.id.age);
+
+
+
+        if(savedInstanceState!=null){
+            String savedText=savedInstanceState.getString(SAVED_TEXT_KEY);
+            name.setText(savedText);
+            age.setText(savedText);
+        }
         }
 
     @Override
@@ -24,8 +34,8 @@ public class From extends AppCompatActivity  {
         super.onSaveInstanceState(savedInstanceState);
 
         //save the state of name and age
-        savedInstanceState.putString("name", name.getText().toString());
-        savedInstanceState.putString("age", age.getText().toString());
+        savedInstanceState.putString(SAVED_TEXT_KEY, name.getText().toString());
+        savedInstanceState.putString(SAVED_TEXT_KEY, age.getText().toString());
     }
 
 }
